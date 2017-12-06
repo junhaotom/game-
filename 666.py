293539d6618e94456c635c8b2742b809950cdccf
850078897 <a850078897@gmail.com>
2017-12-06 08:00:41 -0500

    666
    666

---
+++ 666.py
@@ -0,0 +1,14 @@
+import math
+biggest = max(1,7,10)
+x = abs(3-7)
+def area_sphere(radius):
+    return  4 * math.pi*radius**2
+
+def abs_val(num):
+    if num >= 0:
+        return num
+    else:
+        print("hello")
+        return -num
+
+syval = abs_val(1)
